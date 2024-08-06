2e89dd337b4fd9840ac8d050fab491e38ff3623d
Birat <gotofreelancing1@gmail.com>
2024-08-06 14:13:19 +0545

    Day 2 : Created Login View with Authentication

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from auth_app import views
 
 urlpatterns = [
-    path('', views.display),
+    path('', views.login, name='login'),
 ]
