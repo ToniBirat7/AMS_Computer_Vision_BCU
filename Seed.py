6ceeb83aa1d04567312dd96c5c16a12548070f87
Birat <gotofreelancing1@gmail.com>
2025-12-02 19:33:15 +0545

    update:new working

--- Seed.py
+++
@@ -1,9 +0,0 @@
-students_data = [
-    {"name": "Akshay Kumar", "address": "Mumbai, MH", "age": 27, "phone_number": "9876543210"},
-    {"name": "Alia Bhatt", "address": "Juhu, Mumbai", "age": 24, "phone_number": "9876543211"},
-    {"name": "Amitabh Bachchan", "address": "Jalsa, Mumbai", "age": 30, "phone_number": "9876543212"},
-    {"name": "Anushka Sharma", "address": "Versova, Mumbai", "age": 26, "phone_number": "9876543213"},
-    {"name": "Hrithik Roshan", "address": "Andheri West, Mumbai", "age": 29, "phone_number": "9876543214"},
-    {"name": "Priyanka Chopra", "address": "Bandra, Mumbai", "age": 28, "phone_number": "9876543215"},
-    {"name": "Virat Kohli", "address": "Worli, Mumbai", "age": 25, "phone_number": "9876543216"},
-]
